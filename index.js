0aeb7686660f0088367e67433f62c3d4f620e582
LiHS <lhs19880613@163.com>
2020-07-10 16:42:49 +0800

    新建txt文件

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log("初始化");
 console.log("一些代码逻辑");
 console.log("第二次提交代码");
+//get add .   添加到暂存区  git commite -m 提交仓库  git status 查看状态  git log 查看提交日志（git log --oneline/git reflog）
+console.log("第三次添加的内容");  //git add .  然后 git commit -m "提交"  简化  git commit -a -m "提交到本地仓库"
